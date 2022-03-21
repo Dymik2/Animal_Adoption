d25e67b33fdacdf4545145a73a51957e7efc1b77
Dymik2 <tomaszszewczuk35@gmail.com>
2022-03-21 18:32:03 +0100

    styling mobile

--- src/components/AddNotice.js
+++ src/components/AddNotice.js
@@ -22,18 +22,18 @@ const AddNotice = ({ createAnimal, showAdd }) => {
     return (<div className='addNotice'>
         <form onSubmit={handleSubmit(handleClick)}>
             <input type="text" {...register("type", { required: true, minLength: 3 })} placeholder='Gatunek' />
-            {errors.type?.type === 'required' && <p style={{ color: "red" }}>Type is required</p>}
-            {errors.type?.type === 'minLength' && <p style={{ color: "red" }}>minLength = 3</p>}
+            {errors.type?.type === 'required' && <p style={{ color: "red" }}>Gatunek jest wymagany</p>}
+            {errors.type?.type === 'minLength' && <p style={{ color: "red" }}>Minimalna długość to 3 znaki</p>}
             <input type="text" {...register("race", { required: true, minLength: 3 })} placeholder='Rasa' />
-            {errors.race?.type === 'required' && <p style={{ color: "red" }}>Type is required</p>}
-            {errors.race?.type === 'minLength' && <p style={{ color: "red" }}>minLength = 3</p>}
-            <input type="text" {...register("age", { required: true })} placeholder='wiek' />
-            {errors.age?.type === 'required' && <p style={{ color: "red" }}>Type is required</p>}
+            {errors.race?.type === 'required' && <p style={{ color: "red" }}>Rasa jest wymagana</p>}
+            {errors.race?.type === 'minLength' && <p style={{ color: "red" }}>Minimalna długość to 3 znaki</p>}
+            <input type="text" {...register("age", { required: true })} placeholder='Wiek' />
+            {errors.age?.type === 'required' && <p style={{ color: "red" }}>Wiek jest wymagany</p>}
             <input type="text" {...register("phone", { required: true })} placeholder='Telefon' />
-            {errors.phone?.type === 'required' && <p style={{ color: "red" }}>Type is required</p>}
+            {errors.phone?.type === 'required' && <p style={{ color: "red" }}>Telefon jest wymagany</p>}
             <input type="text" {...register("city", { required: true, minLength: 3 })} placeholder='Miejscowość' />
-            {errors.city?.type === 'required' && <p style={{ color: "red" }}>Type is required</p>}
-            {errors.city?.type === 'minLength' && <p style={{ color: "red" }}>minLength = 3</p>}
+            {errors.city?.type === 'required' && <p style={{ color: "red" }}>Miejscowość jest wymagana</p>}
+            {errors.city?.type === 'minLength' && <p style={{ color: "red" }}>Minimalna długość to 3 znaki</p>}
             <button type="submit">Zapisz</button>
         </form>
     </div >);
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,26 +8,27 @@ const MainContent = ({ animal, deleteAnimal, isFilter, filterList }) => {
     return (
         <div className='mainContent'>
             {!isFilter && localStorage.length > 0 && animal.map((el, index) => {
-                return <div key={index}>
+                return <div key={index} className="notice">
                     <p>Zwierzę: {el.Type}</p>
                     <p>Rasa: {el.Race}</p>
                     <p>Wiek: {el.Age}</p>
                     <p>Telefon: {el.Phone}</p>
                     <p>Miasto: {el.City}</p>
-                    <button disabled={el.nameUser !== localStorage.getItem("savedName")} onClick={() => deleteAnimal(el.id)}>Delete</button>
+                    <button disabled={el.nameUser !== localStorage.getItem("savedName")} onClick={() => deleteAnimal(el.id)}><i className="fa-solid fa-trash-can"></i>
+                    </button>
                     <br />
                     <br />
                 </div>
             })}
 
             {isFilter && localStorage.length > 0 && filterList.map((el, index) => {
-                return <div key={index}>
+                return <div key={index} className="notice">
                     <p>Zwierzę: {el.Type}</p>
                     <p>Rasa: {el.Race}</p>
                     <p>Wiek: {el.Age}</p>
                     <p>Telefon: {el.Phone}</p>
                     <p>Miasto: {el.City}</p>
-                    <button disabled={el.nameUser !== localStorage.getItem("savedName")} onClick={() => deleteAnimal(el.id)}>Delete</button>
+                    <button disabled={el.nameUser !== localStorage.getItem("savedName")} onClick={() => deleteAnimal(el.id)}><i className="fa-solid fa-trash-can"></i></button>
                     <br />
                     <br />
                 </div>
